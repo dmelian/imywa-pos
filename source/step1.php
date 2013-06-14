d8f74b8c5c42fce4c993959ad30cf04bfaeb8ab4
Domingo Melian <domingo@meliangonzalez.com>
2013-06-14 06:40:44 +0100

    update

--- source/step1.php
+++ source/step1.php
@@ -5,52 +5,49 @@ class pos_step1 extends bas_frmx_form{
 		parent::OnLoad();
 		$this->toolbar= new bas_frmx_toolbar('close');
 		$this->title= 'The estheticien';
-		
+
 		$this->buttonbar = new bas_frmx_buttonbar();
-		
-        
-        $groups= new bas_frmx_panelGridQuery("groups",array('width'=>1,'height'=>5));
-        $groups->query->add("item");
-        $groups->query->addcol("id","x","item");
-        $groups->classMain="id";
-        $groups->setEvent("select_group");
-        $groups->setRecord();
-        
-        $items= new bas_frmx_panelGridQuery("items",array('width'=>4,'height'=>5));
-        $items->query->add("item");
-        $items->query->addcol("id","x","item");
-        $items->classMain="id";
-        $items->setEvent("select_item");
-        $items->setRecord();
-        
-        $quantities= new bas_frmx_panelGrid("items",array('width'=>5,'height'=>1));
-        
-        // id,obj,y,x,width,height
-        $frame= new bas_frmx_gridFrame("buttons", array("POS"));
-        $frame->addComponent("group",$groups	,1,1, 1,2);
-        $frame->addComponent("item"	,$items		,1,4, 3,2);
-        $frame->addComponent("qty"	,$quantities,3,1, 4,1);
-        
-        $this->addFrame($frame);
-        
-       
+
+		$groups= new bas_frmx_panelGridQuery("groups",array('width'=>1,'height'=>5));
+		$groups->query->add("item");
+		$groups->query->addcol("item","I","item");
+		$groups->query->addcol("itemGroup", "G");
+		$groups->query->setFilter('main');
+		$groups->classMain="item";
+		$groups->setEvent("select_group");
+		$groups->setRecord();
+
+		$items= new bas_frmx_panelGridQuery("items",array('width'=>4,'height'=>5));
+		$items->query->add("item");
+		$items->query->addcol("item","I","item");
+		$items->query->addcol("itemGroup", "G");
+		$items->query->setFilter('g1');
+		$items->classMain="item";
+		$items->setEvent("select_item");
+		$items->setRecord();
+
+		$quantities= new bas_frmx_panelGrid("items",array('width'=>5,'height'=>1));
+
+		// id,obj,y,x,width,height
+		$frame= new bas_frmx_gridFrame("buttons", array("POS"));
+		$frame->addComponent("group",$groups	,1,1, 1,2);
+		$frame->addComponent("item"	,$items		,1,4, 3,2);
+		$frame->addComponent("qty"	,$quantities,3,1, 4,1);
+
+		$this->addFrame($frame);
 	}
-	
+
 	public function OnAction($action, $data=""){
 		parent::OnAction($action,$data);
 		switch($action){
 			case 'close': return array('close');
 			case 'edit':
-           
-			break;
+
+				break;
 			case 'prevGrid':case 'nextGrid':
-                $this->frames[$data["idFrame"]]->OnAction($action,$data);
-                $this->OnPaint("jscommand");
-			break;
+				$this->frames[$data["idFrame"]]->OnAction($action,$data);
+				$this->OnPaint("jscommand");
+				break;
 		}
 	}
-	
-	
-	
-	
 }
--- sql/pos.data.sql
+++ sql/pos.data.sql
@@ -11,81 +11,94 @@ insert into item(item, type, itemGroup, description, price) values
 	, ('g7' ,'group', @myItemGroup, 'Group 7', null)
 ;
 
+set @myItemGroup='g1';
+set @myGroupDescription='Group 1';
+insert into item(item, type, itemGroup, description, price) values
+	(concat('i1_',@myItemGroup),'item', @myItemGroup, concat('Item 1, ',@myGroupDescription), null)
+	,(concat('i2_',@myItemGroup),'item', @myItemGroup, concat('Item 2, ',@myGroupDescription), null)
+	,(concat('i3_',@myItemGroup),'item', @myItemGroup, concat('Item 3, ',@myGroupDescription), null)
+	,(concat('i4_',@myItemGroup),'item', @myItemGroup, concat('Item 4, ',@myGroupDescription), null)
+	,(concat('i5_',@myItemGroup),'item', @myItemGroup, concat('Item 5, ',@myGroupDescription), null)
+	,(concat('i6_',@myItemGroup),'item', @myItemGroup, concat('Item 6, ',@myGroupDescription), null)
+	,(concat('i7_',@myItemGroup),'item', @myItemGroup, concat('Item 7, ',@myGroupDescription), null)
+	,(concat('i8_',@myItemGroup),'item', @myItemGroup, concat('Item 8, ',@myGroupDescription), null)
+;
+
 set @myItemGroup='g2';
 set @myGroupDescription='Group 2';
 insert into item(item, type, itemGroup, description, price) values
-	(concat('g1_',@myItemGroup),'item', @myItemGroup, concat('Item 1, ',@myGroupDescription), null)
-	,(concat('g2_',@myItemGroup),'item', @myItemGroup, concat('Item 2, ',@myGroupDescription), null)
-	,(concat('g3_',@myItemGroup),'item', @myItemGroup, concat('Item 3, ',@myGroupDescription), null)
-	,(concat('g4_',@myItemGroup),'item', @myItemGroup, concat('Item 4, ',@myGroupDescription), null)
-	,(concat('g5_',@myItemGroup),'item', @myItemGroup, concat('Item 5, ',@myGroupDescription), null)
-	,(concat('g6_',@myItemGroup),'item', @myItemGroup, concat('Item 6, ',@myGroupDescription), null)
-	,(concat('g7_',@myItemGroup),'item', @myItemGroup, concat('Item 7, ',@myGroupDescription), null)
-	,(concat('g8_',@myItemGroup),'item', @myItemGroup, concat('Item 8, ',@myGroupDescription), null)
+	(concat('i1_',@myItemGroup),'item', @myItemGroup, concat('Item 1, ',@myGroupDescription), null)
+	,(concat('i2_',@myItemGroup),'item', @myItemGroup, concat('Item 2, ',@myGroupDescription), null)
+	,(concat('i3_',@myItemGroup),'item', @myItemGroup, concat('Item 3, ',@myGroupDescription), null)
+	,(concat('i4_',@myItemGroup),'item', @myItemGroup, concat('Item 4, ',@myGroupDescription), null)
+	,(concat('i5_',@myItemGroup),'item', @myItemGroup, concat('Item 5, ',@myGroupDescription), null)
+	,(concat('i6_',@myItemGroup),'item', @myItemGroup, concat('Item 6, ',@myGroupDescription), null)
+	,(concat('i7_',@myItemGroup),'item', @myItemGroup, concat('Item 7, ',@myGroupDescription), null)
+	,(concat('i8_',@myItemGroup),'item', @myItemGroup, concat('Item 8, ',@myGroupDescription), null)
 ;
 
 
 set @myItemGroup='g3';
 set @myGroupDescription='Group 3';
 insert into item(item, type, itemGroup, description, price) values
-	(concat('g1_',@myItemGroup),'item', @myItemGroup, concat('Item 1, ',@myGroupDescription), null)
-	,(concat('g2_',@myItemGroup),'item', @myItemGroup, concat('Item 2, ',@myGroupDescription), null)
-	,(concat('g3_',@myItemGroup),'item', @myItemGroup, concat('Item 3, ',@myGroupDescription), null)
-	,(concat('g4_',@myItemGroup),'item', @myItemGroup, concat('Item 4, ',@myGroupDescription), null)
-	,(concat('g5_',@myItemGroup),'item', @myItemGroup, concat('Item 5, ',@myGroupDescription), null)
-	,(concat('g6_',@myItemGroup),'item', @myItemGroup, concat('Item 6, ',@myGroupDescription), null)
-	,(concat('g7_',@myItemGroup),'item', @myItemGroup, concat('Item 7, ',@myGroupDescription), null)
-	,(concat('g8_',@myItemGroup),'item', @myItemGroup, concat('Item 8, ',@myGroupDescription), null)
+	(concat('i1_',@myItemGroup),'item', @myItemGroup, concat('Item 1, ',@myGroupDescription), null)
+	,(concat('i2_',@myItemGroup),'item', @myItemGroup, concat('Item 2, ',@myGroupDescription), null)
+	,(concat('i3_',@myItemGroup),'item', @myItemGroup, concat('Item 3, ',@myGroupDescription), null)
+	,(concat('i4_',@myItemGroup),'item', @myItemGroup, concat('Item 4, ',@myGroupDescription), null)
+	,(concat('i5_',@myItemGroup),'item', @myItemGroup, concat('Item 5, ',@myGroupDescription), null)
+	,(concat('i6_',@myItemGroup),'item', @myItemGroup, concat('Item 6, ',@myGroupDescription), null)
+	,(concat('i7_',@myItemGroup),'item', @myItemGroup, concat('Item 7, ',@myGroupDescription), null)
+	,(concat('i8_',@myItemGroup),'item', @myItemGroup, concat('Item 8, ',@myGroupDescription), null)
 ;
 
 set @myItemGroup='g4';
 set @myGroupDescription='Group 4';
 insert into item(item, type, itemGroup, description, price) values
-	(concat('g1_',@myItemGroup),'item', @myItemGroup, concat('Item 1, ',@myGroupDescription), null)
-	,(concat('g2_',@myItemGroup),'item', @myItemGroup, concat('Item 2, ',@myGroupDescription), null)
-	,(concat('g3_',@myItemGroup),'item', @myItemGroup, concat('Item 3, ',@myGroupDescription), null)
-	,(concat('g4_',@myItemGroup),'item', @myItemGroup, concat('Item 4, ',@myGroupDescription), null)
-	,(concat('g5_',@myItemGroup),'item', @myItemGroup, concat('Item 5, ',@myGroupDescription), null)
-	,(concat('g6_',@myItemGroup),'item', @myItemGroup, concat('Item 6, ',@myGroupDescription), null)
-	,(concat('g7_',@myItemGroup),'item', @myItemGroup, concat('Item 7, ',@myGroupDescription), null)
-	,(concat('g8_',@myItemGroup),'item', @myItemGroup, concat('Item 8, ',@myGroupDescription), null)
+	(concat('i1_',@myItemGroup),'item', @myItemGroup, concat('Item 1, ',@myGroupDescription), null)
+	,(concat('i2_',@myItemGroup),'item', @myItemGroup, concat('Item 2, ',@myGroupDescription), null)
+	,(concat('i3_',@myItemGroup),'item', @myItemGroup, concat('Item 3, ',@myGroupDescription), null)
+	,(concat('i4_',@myItemGroup),'item', @myItemGroup, concat('Item 4, ',@myGroupDescription), null)
+	,(concat('i5_',@myItemGroup),'item', @myItemGroup, concat('Item 5, ',@myGroupDescription), null)
+	,(concat('i6_',@myItemGroup),'item', @myItemGroup, concat('Item 6, ',@myGroupDescription), null)
+	,(concat('i7_',@myItemGroup),'item', @myItemGroup, concat('Item 7, ',@myGroupDescription), null)
+	,(concat('i8_',@myItemGroup),'item', @myItemGroup, concat('Item 8, ',@myGroupDescription), null)
 ;
 
 set @myItemGroup='g5';
 set @myGroupDescription='Group 5';
 insert into item(item, type, itemGroup, description, price) values
-	(concat('g1_',@myItemGroup),'item', @myItemGroup, concat('Item 1, ',@myGroupDescription), null)
-	,(concat('g2_',@myItemGroup),'item', @myItemGroup, concat('Item 2, ',@myGroupDescription), null)
-	,(concat('g3_',@myItemGroup),'item', @myItemGroup, concat('Item 3, ',@myGroupDescription), null)
-	,(concat('g4_',@myItemGroup),'item', @myItemGroup, concat('Item 4, ',@myGroupDescription), null)
-	,(concat('g5_',@myItemGroup),'item', @myItemGroup, concat('Item 5, ',@myGroupDescription), null)
-	,(concat('g6_',@myItemGroup),'item', @myItemGroup, concat('Item 6, ',@myGroupDescription), null)
-	,(concat('g7_',@myItemGroup),'item', @myItemGroup, concat('Item 7, ',@myGroupDescription), null)
-	,(concat('g8_',@myItemGroup),'item', @myItemGroup, concat('Item 8, ',@myGroupDescription), null)
+	(concat('i1_',@myItemGroup),'item', @myItemGroup, concat('Item 1, ',@myGroupDescription), null)
+	,(concat('i2_',@myItemGroup),'item', @myItemGroup, concat('Item 2, ',@myGroupDescription), null)
+	,(concat('i3_',@myItemGroup),'item', @myItemGroup, concat('Item 3, ',@myGroupDescription), null)
+	,(concat('i4_',@myItemGroup),'item', @myItemGroup, concat('Item 4, ',@myGroupDescription), null)
+	,(concat('i5_',@myItemGroup),'item', @myItemGroup, concat('Item 5, ',@myGroupDescription), null)
+	,(concat('i6_',@myItemGroup),'item', @myItemGroup, concat('Item 6, ',@myGroupDescription), null)
+	,(concat('i7_',@myItemGroup),'item', @myItemGroup, concat('Item 7, ',@myGroupDescription), null)
+	,(concat('i8_',@myItemGroup),'item', @myItemGroup, concat('Item 8, ',@myGroupDescription), null)
 ;
 
 set @myItemGroup='g6';
 set @myGroupDescription='Group 6';
 insert into item(item, type, itemGroup, description, price) values
-	(concat('g1_',@myItemGroup),'item', @myItemGroup, concat('Item 1, ',@myGroupDescription), null)
-	,(concat('g2_',@myItemGroup),'item', @myItemGroup, concat('Item 2, ',@myGroupDescription), null)
-	,(concat('g3_',@myItemGroup),'item', @myItemGroup, concat('Item 3, ',@myGroupDescription), null)
-	,(concat('g4_',@myItemGroup),'item', @myItemGroup, concat('Item 4, ',@myGroupDescription), null)
-	,(concat('g5_',@myItemGroup),'item', @myItemGroup, concat('Item 5, ',@myGroupDescription), null)
-	,(concat('g6_',@myItemGroup),'item', @myItemGroup, concat('Item 6, ',@myGroupDescription), null)
-	,(concat('g7_',@myItemGroup),'item', @myItemGroup, concat('Item 7, ',@myGroupDescription), null)
-	,(concat('g8_',@myItemGroup),'item', @myItemGroup, concat('Item 8, ',@myGroupDescription), null)
+	(concat('i1_',@myItemGroup),'item', @myItemGroup, concat('Item 1, ',@myGroupDescription), null)
+	,(concat('i2_',@myItemGroup),'item', @myItemGroup, concat('Item 2, ',@myGroupDescription), null)
+	,(concat('i3_',@myItemGroup),'item', @myItemGroup, concat('Item 3, ',@myGroupDescription), null)
+	,(concat('i4_',@myItemGroup),'item', @myItemGroup, concat('Item 4, ',@myGroupDescription), null)
+	,(concat('i5_',@myItemGroup),'item', @myItemGroup, concat('Item 5, ',@myGroupDescription), null)
+	,(concat('i6_',@myItemGroup),'item', @myItemGroup, concat('Item 6, ',@myGroupDescription), null)
+	,(concat('i7_',@myItemGroup),'item', @myItemGroup, concat('Item 7, ',@myGroupDescription), null)
+	,(concat('i8_',@myItemGroup),'item', @myItemGroup, concat('Item 8, ',@myGroupDescription), null)
 ;
 
 set @myItemGroup='g7';
 set @myGroupDescription='Group 7';
 insert into item(item, type, itemGroup, description, price) values
-	(concat('g1_',@myItemGroup),'item', @myItemGroup, concat('Item 1, ',@myGroupDescription), null)
-	,(concat('g2_',@myItemGroup),'item', @myItemGroup, concat('Item 2, ',@myGroupDescription), null)
-	,(concat('g3_',@myItemGroup),'item', @myItemGroup, concat('Item 3, ',@myGroupDescription), null)
-	,(concat('g4_',@myItemGroup),'item', @myItemGroup, concat('Item 4, ',@myGroupDescription), null)
-	,(concat('g5_',@myItemGroup),'item', @myItemGroup, concat('Item 5, ',@myGroupDescription), null)
-	,(concat('g6_',@myItemGroup),'item', @myItemGroup, concat('Item 6, ',@myGroupDescription), null)
-	,(concat('g7_',@myItemGroup),'item', @myItemGroup, concat('Item 7, ',@myGroupDescription), null)
-	,(concat('g8_',@myItemGroup),'item', @myItemGroup, concat('Item 8, ',@myGroupDescription), null)
+	(concat('i1_',@myItemGroup),'item', @myItemGroup, concat('Item 1, ',@myGroupDescription), null)
+	,(concat('i2_',@myItemGroup),'item', @myItemGroup, concat('Item 2, ',@myGroupDescription), null)
+	,(concat('i3_',@myItemGroup),'item', @myItemGroup, concat('Item 3, ',@myGroupDescription), null)
+	,(concat('i4_',@myItemGroup),'item', @myItemGroup, concat('Item 4, ',@myGroupDescription), null)
+	,(concat('i5_',@myItemGroup),'item', @myItemGroup, concat('Item 5, ',@myGroupDescription), null)
+	,(concat('i6_',@myItemGroup),'item', @myItemGroup, concat('Item 6, ',@myGroupDescription), null)
+	,(concat('i7_',@myItemGroup),'item', @myItemGroup, concat('Item 7, ',@myGroupDescription), null)
+	,(concat('i8_',@myItemGroup),'item', @myItemGroup, concat('Item 8, ',@myGroupDescription), null)
 ;
